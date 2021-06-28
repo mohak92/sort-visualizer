53ea34cbb15302e2101e9b986f16717e27e96f08
mohak92 <tamhane.mohak@gmail.com>
2021-06-27 23:13:07 -0400

    index.ts modifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './Store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ThemeProvider from './components/ThemeContext/ThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
