277ad90cf933e9502e82d7ca8eafaa63c83a80fa
mohak92 <tamhane.mohak@gmail.com>
2021-08-22 22:28:34 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
+import Playground from "./components/Playground";
+
+const AppWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const App: React.FC = () => (
+  <AppWrapper>
+    <Sidebar />
+    <Playground />
+  </AppWrapper>
+);
 
 export default App;
